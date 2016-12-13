ada254953acd7188d152b871ccc8846bbcf85a2d
tomitawataru <tomitawataru@tomitawatarunoMacBook-Pro.local>
2016-12-13 14:58:55 +0900

    Aloha.java while&equals add.

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,8 +1,13 @@
+import java.util.*;
 public class Aloha{
 	public static void main(String[] args) {
-		for(int i=0; i>20; i++){
-			System.out.println("Aloha");
+		Scanner sc = new Scanner(System.in);
+		while(true){
+			String msg = sc.nextLine();
+			System.out.println(msg);
+			if(msg.equals(".")){
+				break;
+			}
 		}
 	}
-	
 }
