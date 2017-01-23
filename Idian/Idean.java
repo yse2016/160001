f20fa02c092319f1948c78e52cd9ce079b081857
tomitawataru <tomitawataru@tomitawatarunoMacBook-Pro.local>
2017-01-23 20:48:33 +0900

    開く、保存完成

--- Idian/Idean.java
+++ Idian/Idean.java
@@ -30,7 +30,7 @@ class Ideaman implements ActionListener{
         btnSave = new JButton("おぼえさせる");
         btnSave.addActionListener(this);
         btnSave.setActionCommand("save"); 
-
+        textArea = new JTextArea(10, 30);
 
         panel.add(btnOpen);
         panel.add(btnSave);
@@ -46,5 +46,42 @@ class Ideaman implements ActionListener{
 	}
 	public void actionPerformed(ActionEvent ae){
 		String cmd = ae.getActionCommand();
+		if(cmd.equals("open")){
+			try {
+				// 1. ファイルを準備する
+				File inFile = new File("data.txt");
+				FileReader fr = new FileReader(inFile);
+				BufferedReader br = new BufferedReader(fr);
+				// 2. ファイルからデータを読み取って表示する
+				String line ;
+				while ((line=br.readLine()) != null) {
+				 	textArea.append(line);
+				 	textArea.append("\n");
+				} 			
+				// 3. ファイルを閉じる
+				br.close();
+			} catch (IOException e) {
+				System.out.println("ファイル読み取りエラー");
+				e.printStackTrace();
+			}
+		}else if (cmd.equals("save")){ 
+			try {
+				// 1. ファイルを準備する
+				File f = new File("result.txt");
+				FileWriter fw = new FileWriter(f);
+				BufferedWriter bw = new BufferedWriter(fw);
+				PrintWriter pw = new PrintWriter(bw);
+				String  data = textArea.getText();
+
+				pw.println(data);
+
+				// 3. ファイルを閉じる
+				pw.close();
+
+				} catch (IOException e) {
+					System.out.println("ファイル書き込みエラー");
+					e.printStackTrace();
+				}
+		}
 	}
 }
